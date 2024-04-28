112f8b772b4286f408f3b28b5769b8fd94b59725
Elie <elie.ad@outlook.com>
2024-04-28 19:37:26 -0400

    still need to test on sim

---
+++ catkin_ws/src/controls/src/sine_control.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import rospy
+from humanoid_msgs.msg import ServoCommand
+import numpy as np
+
+def generate_sine_wave(time):
+    amplitude = 40
+
+    sine_wave = amplitude * np.sin(time)
+
+    return sine_wave
+
+def publish_joint_angles():
+    setpoint_publisher = rospy.Publisher('/servosCommand', ServoCommand, queue_size=1)
+
+    update_interval = 1
+    rate = rospy.Rate(1 / update_interval)
+    x = 0
+    while not rospy.is_shutdown():
+
+        setpoints = ServoCommand()
+        
+        time_offsets = [0, 0, 1.0, 1.0, 2.0, 2.0] 
+        
+        for i, offset in enumerate(time_offsets):
+            sine_value = generate_sine_wave(x + offset)
+            
+            if i == 0:
+                setpoints.left_leg_hip_pitch_setpoint = sine_value 
+            elif i == 1:
+                setpoints.right_leg_hip_pitch_setpoint = sine_value
+            elif i == 2:
+                setpoints.left_leg_knee_setpoint = sine_value 
+            elif i == 3:
+                setpoints.right_leg_knee_setpoint = sine_value 
+            elif i == 4:
+                setpoints.left_leg_ankle_setpoint = sine_value 
+            elif i== 5:
+                setpoints.right_leg_ankle_setpoint = sine_value
+
+        setpoint_publisher.publish(setpoints)
+        x+=0.1
+        rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('sine_control')
+    try:
+        publish_joint_angles()
+    except rospy.ROSInterruptException:
+        pass
