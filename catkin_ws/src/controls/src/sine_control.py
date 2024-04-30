7e366f7ec9b59bf92e8dbb4d1d2f2448b73c954f
Anthony <anthony.tan2@mail.mcgill.ca>
2024-04-30 18:51:21 -0400

    start sine stuff

--- catkin_ws/src/controls/src/sine_control.py
+++ catkin_ws/src/controls/src/sine_control.py
@@ -4,45 +4,40 @@ import rospy
 from humanoid_msgs.msg import ServoCommand
 import numpy as np
 
-def generate_sine_wave(time):
-    amplitude = 40
+def generate_sine_wave(frequency, amplitude, phase):
+    return amplitude * np.sin(2*3.1415*frequency*rospy.get_time() + (3.1415*phase)/180)
 
-    sine_wave = amplitude * np.sin(time)
-
-    return sine_wave
-
-def publish_joint_angles():
+def publish_joint_angles(): 
     setpoint_publisher = rospy.Publisher('/servosCommand', ServoCommand, queue_size=1)
-
-    ros_update_interval = 1
-    rate = rospy.Rate(1 / ros_update_interval)
-    x = 0
+    rate = rospy.Rate(100)
+    setpoints = ServoCommand()
+    setpoints.left_leg_ankle_setpoint = 150
+    setpoints.left_leg_knee_setpoint = 150
+    setpoints.left_leg_hip_roll_setpoint = 150
+    setpoints.left_leg_hip_pitch_setpoint = 150
+    setpoints.left_leg_hip_yaw_setpoint = 150
+    setpoints.right_leg_ankle_setpoint = 150
+    setpoints.right_leg_knee_setpoint = 150
+    setpoints.right_leg_hip_roll_setpoint = 150
+    setpoints.right_leg_hip_pitch_setpoint = 150
+    setpoints.right_leg_hip_yaw_setpoint = 150
+    setpoints.torso_roll_setpoint = 150
+    setpoints.torso_yaw_setpoint = 150
+    setpoints.left_arm_elbow_setpoint = 150
+    setpoints.right_arm_elbow_setpoint = 150
+    setpoints.left_arm_shoulder_setpoint = 150
+    setpoints.right_arm_shoulder_setpoint = 150
+    
     while not rospy.is_shutdown():
-        
-        sine_wave_update = 0.1 # 10 Hz
-
-        setpoints = ServoCommand()
-        
-        time_offsets = [0, 0, 1.0, 1.0, 2.0, 2.0] 
-        
-        for i, offset in enumerate(time_offsets):
-            sine_value = generate_sine_wave(x + offset)
-            
-            if i == 0:
-                setpoints.left_leg_hip_pitch_setpoint = sine_value 
-            elif i == 1:
-                setpoints.right_leg_hip_pitch_setpoint = sine_value
-            elif i == 2:
-                setpoints.left_leg_knee_setpoint = sine_value 
-            elif i == 3:
-                setpoints.right_leg_knee_setpoint = sine_value 
-            elif i == 4:
-                setpoints.left_leg_ankle_setpoint = sine_value 
-            elif i== 5:
-                setpoints.right_leg_ankle_setpoint = sine_value
+    
+        setpoints.left_leg_ankle_setpoint = 150 + generate_sine_wave(rospy.get_param("~ankle_frequency"), rospy.get_param("~ankle_ampl"), rospy.get_param("~left_ankle_phase"))
+        setpoints.left_leg_knee_setpoint = 100 + generate_sine_wave(rospy.get_param("~knee_frequency"), rospy.get_param("~knee_ampl"), rospy.get_param("~left_knee_phase"))
+        setpoints.left_leg_hip_pitch_setpoint = 150 + generate_sine_wave(rospy.get_param("~hip_frequency"), rospy.get_param("~hip_ampl"), rospy.get_param("~left_hip_phase"))
+        setpoints.right_leg_ankle_setpoint = 150 + generate_sine_wave(rospy.get_param("~ankle_frequency"), rospy.get_param("~ankle_ampl"), rospy.get_param("~right_ankle_phase"))
+        setpoints.right_leg_knee_setpoint = 100 + generate_sine_wave(rospy.get_param("~knee_frequency"), rospy.get_param("~knee_ampl"), rospy.get_param("~right_knee_phase"))
+        setpoints.right_leg_hip_pitch_setpoint = 150 + generate_sine_wave(rospy.get_param("~hip_frequency"), rospy.get_param("~hip_ampl"), rospy.get_param("~right_hip_phase"))
 
         setpoint_publisher.publish(setpoints)
-        x+= sine_wave_update
         rate.sleep()
 
 if __name__ == '__main__':
