d31100b739b67611d9dc2ee36d1cc54daa87d2d1
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-06-29 20:13:37 -0400

    update policy control to new robot setup

--- catkin_ws/src/controls/src/policy_control.py
+++ catkin_ws/src/controls/src/policy_control.py
@@ -4,56 +4,50 @@ import rospy
 from humanoid_msgs.msg import ServoCommand
 import numpy as np
 import torch
-from stable_baselines3 import PPO
-from state_generator import PPOStateGenerator, model_output_mapping, joint_limits
-    
+from stable_baselines3 import SAC
+from state_generator import StateGenerator, JOINTS
+
+
 def scaleActionToJointLimits(action):
     scaled_action = []
     for i in range(len(action)):
-        joint_range = joint_limits[model_output_mapping[i]]
+        joint_range = JOINTS.items()[i]
         if action[i] < 0:
             scaled_cmd = 150 + action[i] * joint_range[0]
         else:
             scaled_cmd = 150 + action[i] * joint_range[1]
         scaled_action.append(scaled_cmd)
     return np.array(scaled_action)
-    
+
+
 def generateControl(_):
     obs = stateGenerator.getStateObservation()
     action, _ = ppo_agent.predict(obs, deterministic=True)
-    stateGenerator.updateAction(action)
     scaled_action = scaleActionToJointLimits(action)
-    
+
     setpoints = ServoCommand()
-    setpoints.left_leg_ankle_setpoint = scaled_action[model_output_mapping.index("left_ankle_pitch")]
-    setpoints.left_leg_knee_setpoint = scaled_action[model_output_mapping.index("left_knee")]
-    setpoints.left_leg_hip_roll_setpoint = scaled_action[model_output_mapping.index("left_hip_roll")]
-    setpoints.left_leg_hip_pitch_setpoint = scaled_action[model_output_mapping.index("left_hip_pitch")]
-    setpoints.left_leg_hip_yaw_setpoint = scaled_action[model_output_mapping.index("left_hip_yaw")]
-    setpoints.right_leg_ankle_setpoint = scaled_action[model_output_mapping.index("right_ankle_pitch")]
-    setpoints.right_leg_knee_setpoint = scaled_action[model_output_mapping.index("right_knee")]
-    setpoints.right_leg_hip_roll_setpoint = scaled_action[model_output_mapping.index("right_hip_roll")]
-    setpoints.right_leg_hip_pitch_setpoint = scaled_action[model_output_mapping.index("right_hip_pitch")]
-    setpoints.right_leg_hip_yaw_setpoint = scaled_action[model_output_mapping.index("right_hip_yaw")]
-    setpoints.torso_roll_setpoint = scaled_action[model_output_mapping.index("torso_roll")]
-    setpoints.torso_yaw_setpoint = scaled_action[model_output_mapping.index("torso_yaw")]
-    setpoints.left_arm_elbow_setpoint = scaled_action[model_output_mapping.index("left_elbow")]
-    setpoints.right_arm_elbow_setpoint = scaled_action[model_output_mapping.index("right_elbow")]
-    setpoints.left_arm_shoulder_setpoint = scaled_action[model_output_mapping.index("left_shoulder_pitch")]
-    setpoints.right_arm_shoulder_setpoint = scaled_action[model_output_mapping.index("right_shoulder_pitch")]
-    
+    for i, joint_name in enumerate(JOINTS.keys()):
+        try:
+            setpoints.set_attr(joint_name + "_setpoint", scaled_action[i])
+        except:
+            print("Failed to set setpoint for joint: {}".format(joint_name))
+
     setpoint_publisher.publish(setpoints)
 
-if not torch.cuda.is_available(): print("WARN: Running policy on CPU!")
 
-if __name__ == '__main__':
-    rospy.init_node('policy_controller')
-    setpoint_publisher = rospy.Publisher('/servosCommand', ServoCommand, queue_size=1)
+if not torch.cuda.is_available():
+    print("WARN: Running policy on CPU!")
 
-    stateGenerator = PPOStateGenerator()
+if __name__ == "__main__":
+    rospy.init_node("policy_controller")
+    setpoint_publisher = rospy.Publisher("/servosCommand", ServoCommand, queue_size=1)
 
-    ppo_agent = PPO.load(path=rospy.get_param("~model_checkpoint_path"))
-    
-    timer = rospy.Timer(rospy.Duration(float(rospy.get_param("~control_interval"))), generateControl)
+    stateGenerator = StateGenerator()
+
+    ppo_agent = SAC.load(path=rospy.get_param("~model_checkpoint_path"))
+
+    timer = rospy.Timer(
+        rospy.Duration(float(rospy.get_param("~control_interval"))), generateControl
+    )
 
     rospy.spin()
--- catkin_ws/src/controls/src/state_generator.py
+++ catkin_ws/src/controls/src/state_generator.py
@@ -1,114 +1,97 @@
 import rospy
-from humanoid_msgs.msg import ServoFeedback, PressureSensors
+from humanoid_msgs.msg import ServoFeedback
 from geometry_msgs.msg import Vector3, Quaternion
+from std_msgs.msg import Bool
 import numpy as np
 from scipy.spatial.transform import Rotation
 
-inverseRotateVectors = lambda q, v : Rotation.from_quat([q.x, q.y, q.z, q.w]).inv().apply(v)
+inverseRotateVectors = (
+    lambda q, v: Rotation.from_quat([q.x, q.y, q.z, q.w]).inv().apply(v)
+)
 
 # STATE
-    # joint positions     5 · 16          Joint positions in radians (stacked last 5 timesteps)
-    # angular velocity    5 · 3           Angular velocity (roll, pitch, yaw) from IMU (stacked)
-    # agent velocity      5 · 2           X and Y velocity of robot torso (stacked)
-    # linear acceleration 5 · 3           Linear acceleration from IMU (stacked)
-    # gravity             5 · 3           Gravity direction, derived from angular velocity using Madgwick filter (stacked)
-    # foot pressure       5 · 8           Pressure values from foot sensors (stacked)
-    # previous action     5 · 16          Action filter state (stacked)
-    
+# joint positions                  12          Joint positions in radians (stacked last 5 timesteps)
+# angular velocity                 3           Angular velocity (roll, pitch, yaw) from IMU (stacked)
+# gravity                          3           Gravity direction, derived from angular velocity using Madgwick filter (stacked)
+# binary left foot contact state   1           1 if left foot is in contact with the ground, 0 otherwise
+# binary right foot contact state  1           1 if left foot is in contact with the ground, 0 otherwise
+
 # NOTE: to train, we stack like [joint positions, ....., previous action, joint positions, ..... etc.]
 
-class PPOStateGenerator:
+
+class StateGenerator:
     def __init__(self):
-        self.current_joint_positions = np.array([0] * 16)
-        self.current_lin_accel = np.array([0,0,0])
-        self.current_ang_vel = np.array([0,0,0])
-        self.current_foot_pressures = np.array([0] * 8)
-        self.current_gravity_dir = np.array([0,0,-9.81])
-        self.current_agent_vel = np.array([0, 0])
-        self.previous_action = np.array([0]*16)
-        
-        self.joint_state_sub = rospy.Subscriber('/servosFeedback', ServoFeedback, self.updateJointStates)
-        self.lin_accel_sub = rospy.Subscriber('/state/local_lin_accel', Vector3, self.updateLinAccel)
-        self.ang_vel_sub = rospy.Subscriber('/state/ang_vel', Vector3, self.updateAngVel)
-        self.pressures_sub = rospy.Subscriber('/sensor/pressure_sensors', PressureSensors, self.updatePressure)
-        self.quat_sub = rospy.Subscriber('/state/quat', Quaternion, self.updateQuat)
-        self.vel_sub = rospy.Subscriber('/state/global_vel', Vector3, self.updateVelocity)
-        
+
+        self.current_joint_positions = np.array([0] * len(JOINTS.keys()))
+        self.current_ang_vel = np.array([0, 0, 0])
+        self.current_gravity_dir = np.array([0, 0, -1])
+        self.left_foot_contact = np.array([1])
+        self.right_foot_contact = np.array([1])
+
+        self.joint_state_sub = rospy.Subscriber(
+            "/servosFeedback", ServoFeedback, self.updateJointStates
+        )
+        self.ang_vel_sub = rospy.Subscriber(
+            "/state/ang_vel", Vector3, self.updateAngVel
+        )
+        self.left_con_sub = rospy.Subscriber(
+            "/sensor/contact/left", Bool, self.updateLeftContact
+        )
+        self.right_con_sub = rospy.Subscriber(
+            "/sensor/contact/right", Bool, self.updateRightContact
+        )
+        self.quat_sub = rospy.Subscriber("/state/quat", Quaternion, self.updateQuat)
+
     def updateJointStates(self, msg):
-        try:
-            self.current_joint_positions[model_output_mapping.index("left_ankle_pitch")] = msg.left_leg_ankle_fb[0]
-            self.current_joint_positions[model_output_mapping.index("left_knee")] = msg.left_leg_knee_fb[0]
-            self.current_joint_positions[model_output_mapping.index("left_hip_roll")] = msg.left_leg_hip_roll_fb[0]
-            self.current_joint_positions[model_output_mapping.index("left_hip_pitch")] = msg.left_leg_hip_pitch_fb[0]
-            self.current_joint_positions[model_output_mapping.index("left_hip_yaw")] = msg.left_leg_hip_yaw_fb[0]
-            self.current_joint_positions[model_output_mapping.index("right_ankle_pitch")] = msg.right_leg_ankle_fb[0]
-            self.current_joint_positions[model_output_mapping.index("right_knee")] = msg.right_leg_knee_fb[0]
-            self.current_joint_positions[model_output_mapping.index("right_hip_roll")] = msg.right_leg_hip_roll_fb[0]
-            self.current_joint_positions[model_output_mapping.index("right_hip_pitch")] = msg.right_leg_hip_pitch_fb[0]
-            self.current_joint_positions[model_output_mapping.index("right_hip_yaw")] = msg.right_leg_hip_yaw_fb[0]
-            self.current_joint_positions[model_output_mapping.index("torso_roll")] = msg.torso_roll_fb[0]
-            self.current_joint_positions[model_output_mapping.index("torso_yaw")] = msg.torso_yaw_fb[0]
-            self.current_joint_positions[model_output_mapping.index("left_elbow")] = msg.left_arm_elbow_fb[0]
-            self.current_joint_positions[model_output_mapping.index("right_elbow")] = msg.right_arm_elbow_fb[0]
-            self.current_joint_positions[model_output_mapping.index("left_shoulder_pitch")] = msg.left_arm_shoulder_fb[0]
-            self.current_joint_positions[model_output_mapping.index("right_shoulder_pitch")] = msg.right_arm_shoulder_fb[0]
-        except IndexError:
-            print("WARN: Joint feedback values are incomplete (some servos not sending feedback)!")
-        
-    def updateLinAccel(self, msg):
-        self.current_lin_accel = np.array([msg.x, msg.y, msg.z])
-        
+        joint_positions = []
+        for joint_name in JOINTS.keys():
+            try:
+                joint_pos = msg.get_attr(joint_name + "_fb")
+                joint_positions.append(joint_pos)
+            except:
+                print(
+                    "WARN: Joint feedback values incomplete. Missing {}!".format(
+                        joint_name
+                    )
+                )
+                joint_positions.append(0)
+        self.current_joint_positions = np.array(joint_positions)
+
     def updateAngVel(self, msg):
         self.current_ang_vel = np.array([msg.x, msg.y, msg.z])
-        
-    def updatePressure(self, msg):
-        self.current_foot_pressures[model_pressure_sensor_mapping.index("pressure_geom_RLF")] = msg.right_left_forward
-        self.current_foot_pressures[model_pressure_sensor_mapping.index("pressure_geom_RLB")] = msg.right_left_back
-        self.current_foot_pressures[model_pressure_sensor_mapping.index("pressure_geom_RRF")] = msg.right_right_forward
-        self.current_foot_pressures[model_pressure_sensor_mapping.index("pressure_geom_RRB")] = msg.right_right_back
-        self.current_foot_pressures[model_pressure_sensor_mapping.index("pressure_geom_LLF")] = msg.left_left_forward
-        self.current_foot_pressures[model_pressure_sensor_mapping.index("pressure_geom_LLB")] = msg.left_left_back
-        self.current_foot_pressures[model_pressure_sensor_mapping.index("pressure_geom_LRF")] = msg.left_right_forward
-        self.current_foot_pressures[model_pressure_sensor_mapping.index("pressure_geom_LRB")] = msg.left_right_back
-        
+
+    def updateLeftContact(self, msg):
+        self.left_foot_contact = np.array([1 if msg.data else 0])
+
+    def updateRightContact(self, msg):
+        self.right_foot_contact = np.array([1 if msg.data else 0])
+
     def updateQuat(self, msg):
-        self.current_gravity_dir = inverseRotateVectors(msg, np.array([0,0,-9.81]))
-        
-    def updateVelocity(self, msg):
-        self.current_agent_vel = np.array([msg.x, msg.y])
-        
-    def updateAction(self, action_taken):
-        self.previous_action = action_taken
-    
+        self.current_gravity_dir = inverseRotateVectors(msg, np.array([0, 0, -1]))
+
     def getStateObservation(self):
-        return np.concatenate((self.current_joint_positions,
-                                        self.current_ang_vel,
-                                        self.current_agent_vel,
-                                        self.current_lin_accel,
-                                        self.current_gravity_dir,
-                                        self.current_foot_pressures,
-                                        self.previous_action))
-            
+        return np.concatenate(
+            (
+                self.current_joint_positions,
+                self.current_ang_vel,
+                self.current_gravity_dir,
+                self.left_foot_contact,
+                self.right_foot_contact,
+            )
+        )
 
-model_output_mapping = ["right_shoulder_pitch", "right_elbow", "left_shoulder_pitch", "left_elbow", "left_hip_yaw", "left_hip_roll", "left_hip_pitch", "left_knee", "left_ankle_pitch", "right_hip_yaw", "right_hip_roll", "right_hip_pitch", "right_knee", "right_ankle_pitch", "torso_yaw", "torso_roll"]
 
-model_pressure_sensor_mapping = ["pressure_geom_LLB", "pressure_geom_LRB", "pressure_geom_LRF", "pressure_geom_LLF", "pressure_geom_RLB", "pressure_geom_RRB", "pressure_geom_RRF", "pressure_geom_RLF"]
-
-joint_limits = {
+JOINTS = {
     "right_shoulder_pitch": [-90, 90],
-    "right_elbow": [-90, 90],
+    "right_shoulder_roll": [0, 90],
+    "right_elbow": [-90, 0],
     "left_shoulder_pitch": [-90, 90],
-    "left_elbow": [-90, 90],
-    "left_hip_yaw": [-90, 90],
+    "left_shoulder_roll": [-90, 0],
+    "left_elbow": [-90, 0],
     "left_hip_roll": [-90, 90],
     "left_hip_pitch": [-90, 90],
-    "left_knee": [-90, 90],
-    "left_ankle_pitch": [-90, 90],
-    "right_hip_yaw": [-90, 90],
+    "left_knee": [0, 90],
     "right_hip_roll": [-90, 90],
     "right_hip_pitch": [-90, 90],
-    "right_knee": [-90, 90],
-    "right_ankle_pitch": [-90, 90],
-    "torso_yaw": [-90, 90],
-    "torso_roll": [-90, 90]
+    "right_knee": [0, 90],
 }
