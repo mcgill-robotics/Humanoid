f29e94e6a3f7a1fb48ca327e3aac26011fd5ac50
elieDabdo <elie.ad@outlook.com>
2024-09-22 18:41:16 -0400

    final

--- catkin_ws/src/hardware_interface/src/hardware_interface.py
+++ catkin_ws/src/hardware_interface/src/hardware_interface.py
@@ -1,30 +1,62 @@
 import rospy
-from humanoid_msgs.msg import ServoCommand, SimServoCommand
+import numpy as np
+from humanoid_msgs.msg import HardwareServoCommand, ServoFeedback, HardwareServoFeedback, ServoCommand
 
-# TODO change servo control publisher in unity
-# TODO create mujoco connection to ros
+JOINTS_MAPPINGS_SOFTWARE_HARDWARE = {
+    'right_shoulder_pitch': 'right_shoulder_pitch',
+    'right_shoulder_roll': 'right_shoulder_roll',
+    'right_elbow': 'right_elbow',
+    'left_shoulder_pitch': 'left_shoulder_pitch',
+    'left_shoulder_roll': 'left_shoulder_roll',
+    'left_elbow': 'left_elbow',
+    'left_hip_roll': 'left_hip_roll',
+    'left_hip_pitch': 'left_hip_pitch',
+    'left_knee': 'left_knee',
+    'right_hip_roll': 'right_hip_roll',
+    'right_hip_pitch': 'right_hip_pitch',
+    'right_knee': 'right_knee',
+}
+
+def radians_to_degrees(radians_value):
+    return radians_value * (180.0 / np.pi)
+
+def degrees_to_radians(degrees_value):
+    return degrees_value / (180.0 / np.pi)
+
+def clamp(x, lower_limit, upper_limit):
+    return max(lower_limit, min(x, upper_limit))
 
 def simServoCommandCb(msg):
-    setpoints = ServoCommand()
-    setpoints.left_leg_ankle_setpoint = msg.left_leg_ankle_setpoint
-    setpoints.left_leg_knee_setpoint = msg.left_leg_knee_setpoint
-    setpoints.left_leg_hip_roll_setpoint = msg.left_leg_hip_roll_setpoint
-    setpoints.left_leg_hip_pitch_setpoint = msg.left_leg_hip_pitch_setpoint
-    setpoints.left_leg_hip_yaw_setpoint = msg.left_leg_hip_yaw_setpoint
-    setpoints.right_leg_ankle_setpoint = msg.right_leg_ankle_setpoint
-    setpoints.right_leg_knee_setpoint = msg.right_leg_knee_setpoint
-    setpoints.right_leg_hip_roll_setpoint = msg.right_leg_hip_roll_setpoint
-    setpoints.right_leg_hip_pitch_setpoint = msg.right_leg_hip_pitch_setpoint
-    setpoints.right_leg_hip_yaw_setpoint = msg.right_leg_hip_yaw_setpoint
-    setpoints.torso_roll_setpoint = msg.torso_roll_setpoint
-    setpoints.torso_yaw_setpoint = msg.torso_yaw_setpoint
-    setpoints.left_arm_elbow_setpoint = msg.left_arm_elbow_setpoint
-    setpoints.right_arm_elbow_setpoint = msg.right_arm_elbow_setpoint
-    setpoints.left_arm_shoulder_setpoint = msg.left_arm_shoulder_setpoint
-    setpoints.right_arm_shoulder_setpoint = msg.right_arm_shoulder_setpoint
+    setpoints = HardwareServoCommand()
+
+    for software_joint, hardware_joint in JOINTS_MAPPINGS_SOFTWARE_HARDWARE.items():
+        joint_specs = rospy.getparam(software_joint)
+        direction = joint_specs[2]
+        
+        if direction > 0:
+            upper_joint_limit = joint_specs[0]
+            lower_joint_limit = joint_specs[1]
+        if direction < 0:
+            upper_joint_limit = -joint_specs[1]
+            lower_joint_limit = -joint_specs[0]
+        
+        joint_setpoint = 180 + clamp(direction * radians_to_degrees(getattr(msg, software_joint)), lower_joint_limit, upper_joint_limit)
+        setattr(setpoints, hardware_joint, joint_setpoint)
+
     setpoint_publisher.publish(setpoints)
     
+def simServoFeedbackCb(msg):
+    feedback = ServoFeedback()
     
+    for software_joint, hardware_joint in JOINTS_MAPPINGS_SOFTWARE_HARDWARE.items():
+        joint_specs = rospy.getparam(software_joint)
+        direction = joint_specs[2]
+        
+        joint_feedback = direction * radians_to_degrees(getattr(msg, hardware_joint)-180)
+        setattr(feedback, software_joint, joint_feedback)
+
+    setpoint_publisher.publish(feedback)
+
 
 if __name__ == "__main__":
     rospy.init_node("hardware_interface")
@@ -34,6 +66,6 @@ if __name__ == "__main__":
     feedback_publisher = rospy.Publisher('/servos/feedback', ServoFeedback, queue_size=1)
     # subscribers
     rospy.Subscriber("/servos/command", ServoCommand, simServoCommandCb)
-    rospy.Subscriber("/hardware/servos/feedback", HardwareServoFeedback, simServoCommandCb)
+    rospy.Subscriber("/hardware/servos/feedback", HardwareServoFeedback, simServoFeedbackCb)
 
     rospy.spin()
