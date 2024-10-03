8a1eeb28ca1c27e1e48840be1232ff8a3f107757
antoinedang <antoine.dangeard@gmail.com>
2024-10-03 15:27:51 -0400

    fixes from integration testing

--- clear_port.sh
+++ clear_port.sh
@@ -1,4 +1,4 @@
-processes_on_ports=$(sudo ss -lptn 'sport = :11311')
+processes_on_ports=$(sudo ss -lptn 'sport = :10000')
 
 echo "$processes_on_ports" | sed '1d' | sed -nE 's/.*pid=([0-9]+).*/\1/p' | while read -r pid; do
     sudo kill -9 $pid
